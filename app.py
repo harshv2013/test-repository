008dee0f8344035ed0cbffa5d6eeb3632200f0b0
harshv2013 <harsh2013@gmail.com>
2018-08-21 16:10:14 +0530

    next update

--- app.py
+++ app.py
@@ -9,15 +9,17 @@ from application.resources.user import UserRegister
 from application.security import authenticate, identity
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://harsh:test@localhost:5432/udemy3'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://harsh:test@localhost:5432/udemy4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data3.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
 
 
-
+# @app.before_first_request
+# def create_tables():
+#     db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)  # /auth
@@ -29,6 +31,6 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from application.db import db
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
--- run.py
+++ run.py
@@ -6,3 +6,7 @@ db.init_app(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
+
+
+
+
